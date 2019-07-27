0625e0ba61813e0e38ce72dad07617c081e33e42
MistRay <542035625@qq.com>
2019-07-27 11:34:21 +0800

    操作sql

--- com/study/mysql_handle.py
+++ com/study/mysql_handle.py
@@ -7,7 +7,7 @@ def area(a, b):
 
 def test_database():
     # 打开数据库连接
-    db = pymysql.connect("localhost", "root", "root", "testone")
+    db = pymysql.connect("localhost", "root", "123456", "mysql")
 
     # 使用cursor() 方法创建一个游标对象
     cursor = db.cursor()
@@ -19,31 +19,7 @@ def test_database():
     data = cursor.fetchone()
     print("Database version : %s" % data)
 
-    cursor.execute("select * from t_test_index")
-    # 结果为一个元组
-    test_index = cursor.fetchone()
-
-    ids = test_index[0]
-    print(ids)
-    create_time = test_index[1]
-    print(create_time)
-    create_user = test_index[2]
-    print(create_user)
-    update_time = test_index[3]
-    print(update_time)
-    update_user = test_index[4]
-    print(update_user)
-    order_id = test_index[5]
-    print(order_id)
-    trans_type = test_index[6]
-    print(trans_type)
-    status = test_index[7]
-    print(status)
-    trans_time = test_index[8]
-    print(trans_time)
-    print(test_index)
-    result = area(order_id, status)
-    print(result)
+    # cursor.execute("select * from t_repayment_event limit 1")
     db.close()
 
 
