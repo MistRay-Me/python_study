e47af58a3fdd5ade214fa21b31b76b8fa98b3623
MistRay <542035625@qq.com>
2020-07-22 16:56:14 +0800

    Update mysql_handle.py
    添加详细配置

--- com/study/mysql_handle.py
+++ com/study/mysql_handle.py
@@ -6,8 +6,11 @@ def area(a, b):
 
 
 def test_database():
-    # 打开数据库连接
+    # 打开数据库连接-端口不配置,默认使用3306
     db = pymysql.connect("localhost", "root", "123456", "mysql")
+    
+    # 详细配置
+    # db = pymysql.connect(host="localhost", port=3303, user="readonly", password="readonly", database="mysqldb",charset="utf8")
 
     # 使用cursor() 方法创建一个游标对象
     cursor = db.cursor()
